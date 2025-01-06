54e4fe1bfe9e6c816f8b3d3494052843a367d276
r. ávila <avila.dev@outlook.com>
2025-01-06 11:45:35 -0300

    feat: implement 3 workers /subnet

--- nodes.tf
+++ nodes.tf
@@ -34,12 +34,14 @@ resource "aws_instance" "managers" {
   }
 }
 
-resource "aws_instance" "worker" {
-  subnet_id = aws_subnet.publics[0].id
+resource "aws_instance" "workers" {
+  count = length(aws_subnet.publics) * 3
+
+  subnet_id = aws_subnet.publics[floor(count.index / 3)].id
 
   ami = data.aws_ami.amz_2.id
   instance_type = local.available_type_in_az[
-    aws_subnet.publics[0].availability_zone
+    aws_subnet.publics[floor(count.index / 3)].availability_zone
   ]
 
   key_name                    = aws_key_pair.main.key_name
@@ -55,55 +57,22 @@ resource "aws_instance" "worker" {
     private_key = file("${var.key_name}.pem")
   }
 
+  provisioner "file" {
+    source      = "${var.key_name}.pem"
+    destination = "/home/ec2-user/${var.key_name}.pem"
+  }
+
   provisioner "remote-exec" {
     inline = [
+      "chmod 400 /home/ec2-user/${var.key_name}.pem",
       "sudo service docker start",
       "sudo usermod -a -G docker ec2-user",
-      "JOIN_CMD=$(ssh -o StrictHostKeyChecking=no -i ${var.key_name}.pem ec2-user@${aws_instance.managers[0].public_ip} 'cat /home/ec2-user/swarm_join_cmd.txt')",
+      "JOIN_CMD=$(ssh -o StrictHostKeyChecking=no -i /home/ec2-user/${var.key_name}.pem ec2-user@${aws_instance.managers[floor(count.index / 3)].public_ip} 'cat /home/ec2-user/swarm_join_cmd.txt')",
       "eval $JOIN_CMD"
     ]
   }
 
   tags = {
-    Name = "${var.project_name}-worker-node-1"
+    Name = "${var.project_name}-worker-node-${(floor(count.index / 3) + 1)}.${(count.index % 3 + 1)}"
   }
 }
-
-# resource "aws_instance" "workers" {
-#   count = length(aws_subnet.publics) * 3
-
-#   subnet_id = aws_subnet.publics[floor(count.index / 3)].id
-
-#   ami = data.aws_ami.amz_2.id
-#   instance_type = local.available_type_in_az[
-#     aws_subnet.publics[floor(count.index / 3)].availability_zone
-#   ]
-
-#   key_name                    = aws_key_pair.main.key_name
-#   associate_public_ip_address = true
-#   vpc_security_group_ids = [
-#     aws_security_group.instances.id
-#   ]
-
-#   connection {
-#     host        = self.public_ip
-#     type        = "ssh"
-#     user        = "ec2-user"
-#     private_key = file("${var.key_name}.pem")
-#   }
-
-#   provisioner "remote-exec" {
-#     inline = [
-#       "sudo yum update -y",
-#       "sudo yum install -y docker",
-#       "sudo service docker start",
-#       "sudo usermod -a -G docker ec2-user",
-#       "JOIN_CMD=$(ssh -i ${var.key_name}.pem ec2-user@${aws_instance.managers[floor(count.index / 3)].public_ip} 'cat /home/ec2-user/swarm_join_cmd.txt')",
-#       "eval $JOIN_CMD"
-#     ]
-#   }
-
-#   tags = {
-#     Name = "${var.project_name}-worker-node-${(floor(count.index / 3) + 1)}-${(count.index % 3 + 1)}"
-#   }
-# }
